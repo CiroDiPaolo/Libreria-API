d890f8abe9f6a84d4fc55daf7871969c49eeae4f
Ciro <cirodipaolo15@gmail.com>
2025-05-12 15:10:29 -0300

    programmed class

--- src/main/java/com/LibreriaApi/Control/ReviewCrudController.java
+++ src/main/java/com/LibreriaApi/Control/ReviewCrudController.java
@@ -1,45 +1,61 @@
 package com.LibreriaApi.Control;
 
 import com.LibreriaApi.Model.Review;
+import com.LibreriaApi.Repository.ReviewRepository;
+import com.LibreriaApi.Service.ReviewCrudService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/Review")
+@RequestMapping("/review")
 public class ReviewCrudController {
 
+    @Autowired
+    private ReviewCrudService reviewCrudService;
+
+    //GET
 
     @GetMapping("/{id}")
-    public Review getReviewById(@PathVariable int id) {
+    public Optional<Review> getReviewById(@PathVariable Long id) {
 
-        return null;
+        return reviewCrudService.getReviewByIdService(id);
 
     }
 
     @GetMapping("/all/{}")
-    public Iterable<Review> getAllReviewsOfABook(@PathVariable int id) {
+    public Iterable<Review> getAllReviewsOfABook(@PathVariable Long id) {
 
-        return null;
+        return reviewCrudService.getAllReviewsOfABookService(id);
 
     }
+
+    //DELETE
 
     @DeleteMapping("/{id}")
-    public void deleteReviewById(@PathVariable int id) {
+    public void deleteReviewById(@PathVariable Long id) {
 
+        reviewCrudService.deleteByIdService(id);
 
     }
+
+    //POST
 
     @PostMapping()
     public void addReview(@RequestBody Review review) {
 
+        reviewCrudService.addReviewService(review);
 
     }
 
-    @PutMapping
+    //PUT
+
+    @PutMapping()
     public void updateReview(@RequestBody Review review) {
 
+        reviewCrudService.updateReviewService(review);
 
     }
-
-
 
 }
