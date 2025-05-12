3b78d1dac4b6a0265036e8d0097d572bbacc2e12
Ciro <cirodipaolo15@gmail.com>
2025-05-12 13:36:20 -0300

    create class ReviewCrudController

---
+++ src/main/java/com/LibreriaApi/Control/ReviewCrudController.java
@@ -0,0 +1,15 @@
+package com.LibreriaApi.Control;
+
+import com.LibreriaApi.Model.Review;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Review")
+public class ReviewCrudController {
+
+
+
+}
