dd4396dcadde8c9c01423e4c7a1d045b8da3f516
Ciro <cirodipaolo15@gmail.com>
2025-05-12 13:38:38 -0300

    programmed class ReviewCrudController

--- src/main/java/com/LibreriaApi/Control/ReviewCrudController.java
+++ src/main/java/com/LibreriaApi/Control/ReviewCrudController.java
@@ -1,14 +1,44 @@
 package com.LibreriaApi.Control;
 
 import com.LibreriaApi.Model.Review;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Review")
 public class ReviewCrudController {
+
+
+    @GetMapping("/{id}")
+    public Review getReviewById(@PathVariable int id) {
+
+        return null;
+
+    }
+
+    @GetMapping("/all/{}")
+    public Iterable<Review> getAllReviewsOfABook(@PathVariable int id) {
+
+        return null;
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReviewById(@PathVariable int id) {
+
+
+    }
+
+    @PostMapping()
+    public void addReview(@RequestBody Review review) {
+
+
+    }
+
+    @PutMapping
+    public void updateReview(@RequestBody Review review) {
+
+
+    }
 
 
 
