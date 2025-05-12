1bc0ea83de0623045582499bbecdfc410a770c13
Ciro <cirodipaolo15@gmail.com>
2025-05-12 15:10:12 -0300

    clean code

--- src/main/java/com/LibreriaApi/Control/RegisterController.java
+++ src/main/java/com/LibreriaApi/Control/RegisterController.java
@@ -4,10 +4,7 @@ import com.LibreriaApi.Model.User;
 import com.LibreriaApi.Service.RegisterService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/register")
@@ -16,7 +13,7 @@ public class RegisterController {
     @Autowired
     private RegisterService registerService;
 
-    @PutMapping("/")
+    @PostMapping()
     public void register(@RequestBody @Valid User user) {
 
         registerService.registerService(user);
