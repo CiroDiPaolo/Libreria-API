0036d63e2082cd9ee5d9601855de403139298def
Melisa Carrizo <melisacarrizo5@gmail.com>
2025-05-13 15:20:48 -0300

    added book validations

--- src/main/java/com/LibreriaApi/Model/Book.java
+++ src/main/java/com/LibreriaApi/Model/Book.java
@@ -3,6 +3,8 @@ package com.LibreriaApi.Model;
 import com.LibreriaApi.Enums.Category;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -15,21 +17,23 @@ import java.util.SimpleTimeZone;
 public class Book extends Multimedia{
 
     @NotNull(message = "El ISBN no puede ser nulo")
-    @Column(name = "ISBN",length = 20)
+    @Pattern(regexp = "^(\\d{9}[0-9X])|(978\\d{9}[0-9])$", message = "El ISBN debe tener 10 o 13 caracteres")
+    @Column(name = "ISBN",length = 13)
     private String ISBN;
 
     @NotNull(message = "El titulo no puede ser nulo")
-    @Size(max = 100, message = "El titulo no debe exdecer los 100 caracteres")
+    @Size(min = 1, max = 100, message = "El titulo no debe exdecer los 100 caracteres")
     @Column(name = "title",length = 100)
     private String title;
 
     @NotNull(message = "El autor no puede ser nulo")
-    @Size(max = 30, message = "El autor no debe exdecer los 30 caracteres")
+    @Size(min = 1, max = 30, message = "El autor no debe exdecer los 30 caracteres")
     @Column(name = "author",length = 30)
     private String author;
 
     @NotNull(message = "La editorial no puede ser nula")
-    @Size(max = 30, message = "La editorial no debe exdecer los 30 caracteres")
+    @Size(min = 1, max = 30, message = "La editorial no debe exdecer los 30 caracteres")
+    @PastOrPresent(message = "La fecha de publicación debe ser en el pasado o el presente")
     @Column(name = "publishingHouse",length = 30)
     private String publishingHouse;
 
