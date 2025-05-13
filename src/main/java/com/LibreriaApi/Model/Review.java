8dc6570dbfd446b61c48107e68dbb2d736643cbf
Melisa Carrizo <melisacarrizo5@gmail.com>
2025-05-13 16:02:53 -0300

    Fix review for JPA

--- src/main/java/com/LibreriaApi/Model/Review.java
+++ src/main/java/com/LibreriaApi/Model/Review.java
@@ -1,28 +1,36 @@
 package com.LibreriaApi.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "Review")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Builder
 public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idReview")
     private Long idReview;
 
+    @Column(name = "rating")
     private int rating;
 
+    @Column(name = "content", length = 250)
     private String content;
 
+    @Column(name = "status")
     private Boolean status;
 
     @ManyToOne
-    @JoinColumn(name = "idUsuario")
+    @JoinColumn(name = "idUser")
     private User user;
 
     @ManyToOne
