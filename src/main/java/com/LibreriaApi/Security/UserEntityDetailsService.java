bc70410cebc08b94100152d0cbde2c64976d745d
Castro Lautaro <castrolautaro235@gmail.com>
2025-05-13 22:55:38 -0300

    Create UserEntityDetails and UserEntityDetailsService.

---
+++ src/main/java/com/LibreriaApi/Security/UserEntityDetailsService.java
@@ -0,0 +1,26 @@
+package com.LibreriaApi.Security;
+
+import com.LibreriaApi.Model.UserEntity;
+import com.LibreriaApi.Repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserEntityDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserEntityDetailsService(UserRepository userRepository) {
+        this.userRepository= userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity usuario = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+        return new UserEntityDetails(usuario);
+    }
+
+}
