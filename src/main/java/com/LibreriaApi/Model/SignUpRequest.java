e0bdddfe2b6d90c992b4310b1186f2e6f80e6b42
Castro Lautaro <castrolautaro235@gmail.com>
2025-05-26 14:03:58 -0300

    Create class SignUpRequest

---
+++ src/main/java/com/LibreriaApi/Model/SignUpRequest.java
@@ -0,0 +1,24 @@
+package com.LibreriaApi.Model;
+
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class SignUpRequest {
+
+    @NotBlank(message = "El nombre de usuario no puede ser nulo")
+    @Size(min = 2, max = 20, message = "El usuario debe tener entre 2 y 20 caracteres")
+    private String username;
+
+    @NotNull(message = "El email no puede ser nulo")
+    @Email(message = "El email debe ser valido")
+    @Size(max = 30, message = "El email no debe exceder los 30 caracteres")
+    private String email;
+
+    @NotNull(message = "La contraseña no puede ser nula")
+    @Size(min = 6, max = 20, message = "La contraseña debe tener entre 6 y 20 caracteres")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$", message = "La contraseña debe tener al menos una mayúscula, una minúscula y un número")
+    private String password;
+
+}
