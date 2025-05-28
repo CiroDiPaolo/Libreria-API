e86238a87d1d046a3e624ac270ffb7b0876decec
Melisa Carrizo <melisacarrizo5@gmail.com>
2025-05-28 20:31:57 -0300

    Changed return of findByMultimedia_Id and deleted overriden method findById

--- src/main/java/com/LibreriaApi/Repository/ReviewRepository.java
+++ src/main/java/com/LibreriaApi/Repository/ReviewRepository.java
@@ -4,12 +4,12 @@ import com.LibreriaApi.Model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review , Long> {
 
-        Iterable<Review> findByMultimedia_Id(Long id);
+        List<Review> findByMultimedia_Id(Long id);
 
-        Optional<Review> findById(Long id);
 }
