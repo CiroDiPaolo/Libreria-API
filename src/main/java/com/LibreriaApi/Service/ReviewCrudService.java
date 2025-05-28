557cf8d1c7d0654f71e31103e1784257e7055086
Melisa Carrizo <melisacarrizo5@gmail.com>
2025-05-28 20:32:15 -0300

    Changed methods

--- src/main/java/com/LibreriaApi/Service/ReviewCrudService.java
+++ src/main/java/com/LibreriaApi/Service/ReviewCrudService.java
@@ -1,11 +1,14 @@
 package com.LibreriaApi.Service;
 
+import com.LibreriaApi.Exceptions.EntityNotFoundException;
 import com.LibreriaApi.Model.Review;
+import com.LibreriaApi.Repository.BookRepository;
 import com.LibreriaApi.Repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,22 +17,27 @@ public class ReviewCrudService {
     @Autowired
     private ReviewRepository reviewRepository;
 
+    @Autowired
+    private BookRepository bookRepository;
+
     //Metodos GET
 
-    public Optional<Review> getReviewByIdService(Long id) {
+    public Review getReviewByIdService(Long id) {
 
-        return reviewRepository.findById(id);
+        return reviewRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Review no encontrada con id: " + id));
 
     }
 
-    public Iterable<Review> getAllReviewsOfABookService(Long id) {
-
-        return reviewRepository.findByMultimedia_Id(id);
-
+    public List<Review> getAllReviewsOfABookService(Long bookId) {
+        if (!bookRepository.existsById(bookId)) {
+            throw new EntityNotFoundException("Libro no encontrado con id: " + bookId);
+        }
+        return reviewRepository.findByMultimedia_Id(bookId);
     }
 
     //Metodos DELETE
-    public void deleteByIdService(@PathVariable Long id) {
+    public void deleteByIdService(Long id) {
 
         Optional<Review> op = reviewRepository.findById(id);
 
@@ -37,40 +45,26 @@ public class ReviewCrudService {
 
             reviewRepository.deleteById(id);
 
+        }else{
+            throw new EntityNotFoundException("Review no encontrada");
         }
 
     }
 
     //Metodo PUT
 
-    public void addReviewService(Review review) {
-
-        reviewRepository.save(review);
-
+    public Review addReviewService(Review review) {
+        return reviewRepository.save(review);
     }
 
     //Meteodo POST
 
-    public void updateReviewService(Review review) {
-
-        Optional<Review> op = reviewRepository.findById(review.getIdReview());
-
-        if(op.isPresent()){
-
-            Review updatedReview = op.get();
-
-            if(updatedReview.getIdReview() == review.getIdReview() ||
-                    updatedReview.getMultimedia() == review.getMultimedia() ||
-                    updatedReview.getUser() == review.getUser()){
-
-                reviewRepository.save(review);
-
-
-            }
-
+    public Review updateReviewService(Long id, Review review) {
+        if (!reviewRepository.existsById(id)) {
+            throw new EntityNotFoundException("Review no encontrada");
         }
-
+        review.setIdReview(id); 
+        return reviewRepository.save(review);
     }
-
 
 }
