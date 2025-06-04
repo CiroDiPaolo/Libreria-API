a3887d5cc33703935a4a888fa2682b52b01cd8d4
Ciro <cirodipaolo15@gmail.com>
2025-06-04 06:20:58 -0300

    Desing class BookStage

--- src/main/java/com/LibreriaApi/Model/BookStage.java
+++ src/main/java/com/LibreriaApi/Model/BookStage.java
@@ -1,24 +1,27 @@
 package com.LibreriaApi.Model;
 
 import com.LibreriaApi.Enums.Stage;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Table(name = "BookStage")
 @Data
 public class BookStage {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idStage")
     private Long id;
 
-    @NotNull(message = "El usuario no puede ser nulo")
     @ManyToOne
-    @JoinColumn(name = "idUser", referencedColumnName = "idUser")
-    @Valid
+    @JsonBackReference
+    @JoinColumn(name = "idUser")
     private UserEntity user;
 
     @NotNull(message = "El libro no puede ser nulo")
