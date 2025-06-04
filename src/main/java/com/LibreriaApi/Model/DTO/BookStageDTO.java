fb1a7ceebdc11fb3ba7ecb720991edf0cdfe478f
Ciro <cirodipaolo15@gmail.com>
2025-06-04 06:21:41 -0300

    create class BookStageDTO

--- src/main/java/com/LibreriaApi/Model/DTO/BookStageDTO.java
+++ src/main/java/com/LibreriaApi/Model/DTO/BookStageDTO.java
@@ -1,4 +1,26 @@
 package com.LibreriaApi.Model.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BookStageDTO {
+
+    @NotBlank(message = "El id del libro no puede ser nulo")
+    @Positive(message = "El ID de libro debe ser positivo")
+
+    private Long idBook;
+
+    @NotBlank(message = "El id del usuario no puede ser nulo")
+    @Positive(message = "El ID de libro debe ser positivo")
+    private Long idUser;
+
+
 }
