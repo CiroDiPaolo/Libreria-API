cb7f8d089f01d96b4c10de8feab43e9e6b28dd88
Ciro <cirodipaolo15@gmail.com>
2025-06-05 17:47:28 -0300

    rename class

--- src/main/java/com/LibreriaApi/Model/DTO/BookStageDTO.java
+++ src/main/java/com/LibreriaApi/Model/DTO/BookStageDTO.java
@@ -11,11 +11,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class BookIdDTO {
+public class BookStageDTO {
 
     @NotBlank(message = "El id del libro no puede ser nulo")
     @Positive(message = "El ID de libro debe ser positivo")
-
     private Long idBook;
-    
+
+    @NotBlank(message = "El id del usuario no puede ser nulo")
+    @Positive(message = "El ID de libro debe ser positivo")
+    private Long idUser;
+
+
 }
