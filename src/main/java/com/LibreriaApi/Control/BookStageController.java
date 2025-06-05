d52e5070968da2c2a6892b65b7c842f479cdb142
Ciro <cirodipaolo15@gmail.com>
2025-06-05 15:34:23 -0300

    programmed method post for BookStageController

--- src/main/java/com/LibreriaApi/Control/BookStageController.java
+++ src/main/java/com/LibreriaApi/Control/BookStageController.java
@@ -1,14 +1,10 @@
 package com.LibreriaApi.Control;
 
-import com.LibreriaApi.Model.Book;
 import com.LibreriaApi.Model.BookStage;
-import com.LibreriaApi.Model.DTO.BookStageDTO;
-import com.LibreriaApi.Model.UserEntity;
-import com.LibreriaApi.Repository.UserRepository;
+import com.LibreriaApi.Model.DTO.BookIdDTO;
 import com.LibreriaApi.Service.BookStageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,23 +14,11 @@ public class BookStageController {
     @Autowired
     private BookStageService bookStageService;
 
-    @Autowired
-    private BookCrudController bookCrudController;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<BookStage> createBookStage(@RequestBody BookStageDTO bookStageDTO) {
+    public ResponseEntity<BookStage> createBookStage(@RequestBody BookIdDTO bookStageDTO) {
 
-        System.out.println("prueba");
-
-        Book book = bookCrudController.getBook(bookStageDTO.getIdBook());
-
-        UserEntity user = userRepository.getById(bookStageDTO.getIdUser());
-
-        return ResponseEntity.ok(bookStageService.createService(user,book));
+        return ResponseEntity.ok(bookStageService.createService(bookStageDTO));
     }
 
 
