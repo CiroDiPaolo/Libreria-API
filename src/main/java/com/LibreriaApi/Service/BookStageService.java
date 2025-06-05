167f5c9d6033335f92127bad1be9c0d8851510cc
Ciro <cirodipaolo15@gmail.com>
2025-06-05 15:35:05 -0300

    programmed method createService

--- src/main/java/com/LibreriaApi/Service/BookStageService.java
+++ src/main/java/com/LibreriaApi/Service/BookStageService.java
@@ -1,17 +1,17 @@
 package com.LibreriaApi.Service;
 
+import com.LibreriaApi.Control.BookCrudController;
 import com.LibreriaApi.Enums.Stage;
 import com.LibreriaApi.Exceptions.BookStageNotFoundException;
 import com.LibreriaApi.Model.Book;
 import com.LibreriaApi.Model.BookStage;
+import com.LibreriaApi.Model.DTO.BookIdDTO;
 import com.LibreriaApi.Model.UserEntity;
 import com.LibreriaApi.Repository.BookStageRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import com.LibreriaApi.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,19 +20,39 @@ public class BookStageService {
     @Autowired
     private BookStageRepository bookStageRepository;
 
-    public BookStage createService(UserEntity user, Book book){
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BookCrudController bookCrudController;
+
+    //POST
+
+    public BookStage createService(BookIdDTO bookStageDTO) {
+
+        Long userId = userService.getIdUserByToken();
+
+        Optional<UserEntity> user = userRepository.findById(userId);
+
+        Book book = bookCrudController.getBook(bookStageDTO.getIdBook());
+
+        bookStageRepository.findByUserAndBook(user.orElse(null), book)
+                .ifPresent(bs -> {
+                    throw new BookStageNotFoundException("Este usuario ya tiene este libro en favoritos");
+                });
 
         BookStage bookStage = new BookStage();
 
         bookStage.setBook(book);
 
-        bookStage.setUser(user);
+        bookStage.setUser(user.get());
 
         bookStage.setStage(Stage.PENDIENTE);
 
         return bookStageRepository.save(bookStage);
-
     }
-
 
 }
