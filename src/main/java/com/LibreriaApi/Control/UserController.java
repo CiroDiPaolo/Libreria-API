73d64abfab1f172a93b12848aa70e7fc88d0b9d5
Ciro <cirodipaolo15@gmail.com>
2025-06-06 19:21:59 -0300

    add method post

--- src/main/java/com/LibreriaApi/Control/UserController.java
+++ src/main/java/com/LibreriaApi/Control/UserController.java
@@ -1,5 +1,6 @@
 package com.LibreriaApi.Control;
 
+import com.LibreriaApi.Model.DTO.UserEntityDTO;
 import com.LibreriaApi.Model.UserEntity;
 import com.LibreriaApi.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,14 @@ public class UserController {
         userService.deleteUserById(idUser);
 
         return ResponseEntity.noContent().build();
+
+    }
+
+    //metodo post
+    @PostMapping()
+    public ResponseEntity<UserEntity> updateUser(@RequestBody UserEntityDTO user){
+
+        return ResponseEntity.ok(userService.updateUser(user));
 
     }
 
