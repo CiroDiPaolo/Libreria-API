dfd92173663379d5586f11c4596cc1b4c48eddfe
Melisa Carrizo <melisacarrizo5@gmail.com>
2025-06-07 00:24:02 -0300

    Added methods for searching

--- src/main/java/com/LibreriaApi/Repository/BookRepository.java
+++ src/main/java/com/LibreriaApi/Repository/BookRepository.java
@@ -2,8 +2,11 @@ package com.LibreriaApi.Repository;
 
 import com.LibreriaApi.Model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,6 +16,11 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByISBN(String ISBN);
     Optional<Book> findByAuthor(String author);
     Optional<Book> findBypublishingHouse(String publishingHouse);
-
+    @Query("SELECT b FROM Book b WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))")
+    List<Book> searchByTitleLikeIgnoreCase(@Param("title") String title);
+    @Query("SELECT b FROM Book b WHERE LOWER(b.author) LIKE LOWER(CONCAT('%', :author, '%'))")
+    List<Book> searchByAuthorLikeIgnoreCase(@Param("author") String author);
+    @Query("SELECT b FROM Book b WHERE LOWER(b.publishingHouse) LIKE LOWER(CONCAT('%', :publishingHouse, '%'))")
+    List<Book> searchByPublishinHouseLikeIgnoreCase(@Param("publishingHouse") String publishingHouse);
 
 }
