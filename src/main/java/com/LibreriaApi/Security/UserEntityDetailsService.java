ea1acfbac421e5c0ac5ae1df018bbab1ee25442e
Castro Lautaro <castrolautaro235@gmail.com>
2025-06-12 13:21:48 -0300

    Add validation to the user status in login controller

--- src/main/java/com/LibreriaApi/Security/UserEntityDetailsService.java
+++ src/main/java/com/LibreriaApi/Security/UserEntityDetailsService.java
@@ -1,7 +1,9 @@
 package com.LibreriaApi.Security;
 
+import com.LibreriaApi.Exceptions.AccessDeniedUserException;
 import com.LibreriaApi.Model.UserEntity;
 import com.LibreriaApi.Repository.UserRepository;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,8 +20,11 @@ public class UserEntityDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // TRAIGO AL USUARIO POR SU EMAIL
         UserEntity usuario = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+
         return new UserEntityDetails(usuario);
     }
 
