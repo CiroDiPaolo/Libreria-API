6551d4facb9525ed3fc43cd99ce51180dea89795
Castro Lautaro <castrolautaro235@gmail.com>
2025-06-12 13:37:26 -0300

    Add AuthService class to separate the logic from AuthController

---
+++ src/main/java/com/LibreriaApi/Service/AuthService.java
@@ -0,0 +1,22 @@
+package com.LibreriaApi.Service;
+
+import com.LibreriaApi.Config.JwtUtil;
+import com.LibreriaApi.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtUtil jwtUtil;
+
+}
