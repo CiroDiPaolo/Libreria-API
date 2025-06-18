62df1eaf1abdecddf0db52e5f08d896db5027b17
Castro Lautaro <castrolautaro235@gmail.com>
2025-06-17 21:39:36 -0300

    Create method createBookWithAPI in BookService

--- src/main/java/com/LibreriaApi/Service/BookService.java
+++ src/main/java/com/LibreriaApi/Service/BookService.java
@@ -1,11 +1,13 @@
 package com.LibreriaApi.Service;
 
+import com.LibreriaApi.Enums.Category;
 import com.LibreriaApi.Exceptions.AccessDeniedUserException;
 import com.LibreriaApi.Exceptions.EntityAlreadyExistsException;
 import com.LibreriaApi.Exceptions.EntityNotFoundException;
 import com.LibreriaApi.Model.Book;
 import com.LibreriaApi.Model.DTO.BookDTO;
 import com.LibreriaApi.Model.DTO.BookWithReviewsDTO;
+import com.LibreriaApi.Model.DTO.LoadBookDTO;
 import com.LibreriaApi.Model.DTO.ReviewDTO;
 import com.LibreriaApi.Model.Review;
 import com.LibreriaApi.Repository.BookRepository;
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -88,6 +91,46 @@ public class BookService {
 
         return bookRepository.save(book);
 
+    }
+
+    // USA UN LoadBookDTO PARA COMPLETAR EL RESTO DE LOS DATOS CON LA API
+    // OBTIENE TODOS LOS DATOS EN BASE AL ISBN
+    @Transactional
+    public Book addBookWithAPI(LoadBookDTO dto) {
+        // VALIDO QUE EL ISBN NO ESTE REGISTRADO AÚN
+        if(bookRepository.existsByISBN(dto.getIsbn())){
+            throw new EntityAlreadyExistsException("Ya existe un libro con el ISBN: " + dto.getIsbn());
+        }
+        // TRAIGO UN BookInfo, QUE CONTIENE LA INFORMACION OBTENIDA DE LA API
+        Optional<GoogleBooksRequeast.BookInfo> bookInfoOpt = googleApi.getBookInfoByISBN(dto.getIsbn());
+        // SI EL BookInfo ESTA VACIO EL METODO ARROJA UNA EXCEPCION
+        /*
+        if (bookInfo.isEmpty()) {
+            throw new IllegalArgumentException("No se encontró información en Google Books para el ISBN: " + dto.getIsbn());
+        }
+        */
+        // OBTENGO LA URL DE LA PORTADA
+        String url = googleApi.getThumbnailByISBN(dto.getIsbn());
+        GoogleBooksRequeast.BookInfo info = bookInfoOpt.get();
+
+
+        Book book = new Book();
+        // SETEO LOS DATOS QUE VIENEN DEL DTO
+        book.setISBN(dto.getIsbn());
+        book.setCategory(Category.valueOf(dto.getCategory()));
+        book.setAuthor(dto.getAuthor());
+        book.setDescription(dto.getDescription());
+        // SETEO LOS DATOS QUE OBTENGO CON LA API
+        book.setTitle(info.getTitle());
+        book.setPublishingHouse(info.getPublisher());
+        if (info.getDate() != null) {
+            book.setReleaseDate(Date.valueOf(info.getDate()));
+        }
+        book.setUrlImage(url);
+        // SETEO EL ESTADO
+        book.setStatus(true);
+
+        return bookRepository.save(book);
     }
 
     //METODO UPDATE
