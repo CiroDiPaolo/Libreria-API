26afb79b8b6d9564df3c439bc42ea9bc631dab5b
Melisa Carrizo <melisacarrizo5@gmail.com>
2026-01-14 13:18:29 -0300

    fix(pagination): adapt findByMultimediaIdAndStatusTrue to support pagination

--- src/main/java/com/LibreriaApi/Repository/ReviewRepository.java
+++ src/main/java/com/LibreriaApi/Repository/ReviewRepository.java
@@ -1,6 +1,9 @@
 package com.LibreriaApi.Repository;
 
+import com.LibreriaApi.Model.DTO.ReviewDTO;
 import com.LibreriaApi.Model.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -20,7 +23,7 @@ public interface ReviewRepository extends JpaRepository<Review , Long> {
 
         boolean existsByUser_IdAndStatusTrueAndMultimedia_Id(Long idUser, Long multimediaId);
 
-        List<Review> findByMultimediaIdAndStatusTrue(Long idMultimedia);
+        Page<Review> findByMultimedia_IdAndStatusTrue(Long idMultimedia, Pageable pageable);
         Optional<Review> findByMultimediaIdAndUserIdAndStatusTrue(Long idMultimedia, Long idUser);
 
         Optional<Review> findByMultimediaIdAndUserId(Long idMultimedia, Long idUser);
